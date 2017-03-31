3384cea23df344003345458f78acf76b03a892ea
Heather Taylor <heather@heysparkbox.com>
2017-03-31 14:17:36 -0400

    feat: add function to random.js
    add function with 2 parameters to random.js

--- Random Number Challenge/random.js
+++ Random Number Challenge/random.js
@@ -1,7 +1,21 @@
-var input1 = prompt("Please type a starting number");
-var bottomNumber = parseInt(input1);
-var input = prompt("Please type a number");
-var topNumber = parseInt(input);
-var randomNumber = Math.floor(Math.random() * (topNumber - bottomNumber +1)) + bottomNumber;
-var message = "<p>" + randomNumber + " is a number between " + bottomNumber + " and " + topNumber + ".</p>";
-document.write(message);
+// var input1 = prompt("Please type a starting number");
+// var bottomNumber = parseInt(input1);
+// var input = prompt("Please type a number");
+// var topNumber = parseInt(input);
+// var randomNumber = Math.floor(Math.random() * (topNumber - bottomNumber +1)) + bottomNumber;
+// var message = "<p>" + randomNumber + " is a number between " + bottomNumber + " and " + topNumber + ".</p>";
+// document.write(message);
+
+// Random Number Challenge - take 2 using a function
+function getRandomNumber (lower, upper) {
+  if (isNaN(lower) || isNaN(upper)) {
+    throw new Error('Both arguments must be numbers');
+  }
+  return Math.floor(Math.random() * (upper - lower + 1)) + lower;
+}
+
+console.log(getRandomNumber(1,6));
+console.log(getRandomNumber(1, 100));
+console.log(getRandomNumber(200, 500));
+console.log(getRandomNumber(50, 100));
+//console.log(getRandomNumber('nine', 59)); // error thrown and program stops
